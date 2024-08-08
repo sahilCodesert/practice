59e7df6d689e1a58e4e376902fdfab041289cddb
sahil <shahilchangela@gmail.com>
2024-08-08 17:00:30 +0530

    create file pcrud

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Crud from "./componente/Crud";
+import Pcrud from "./componente/Pcrud";
 
 function App() {
   return (
     <div>
       <Crud />
+      <Pcrud />
     </div>
   );
 }
